5d3acb2092b394356499d04e59782cc9dda7e166
pavithra5B1-27 <kenchugondepavi27@gmail.com>
2025-11-27 15:12:53 +0530

    asciivalue.c

--- asciivalue.c
+++ asciivalue.c
@@ -5,4 +5,8 @@ int main()
     printf("Enter a character: ");
     scanf("%c",&c);
     printf("ASCII value of %c = %d", c,c);
+    int n;
+    printf("Enter a number: ");
+    scanf("%d",&n);
+    printf("ASCII value of %d = %c", n,n);
 }
