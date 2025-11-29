ad6fea543afd109c98def457d18ddab4e6230266
pavithra5B1-27 <kenchugondepavi27@gmail.com>
2025-11-29 09:08:43 +0530

    power .c

--- power .c
+++ power .c
@@ -14,3 +14,22 @@ int main()
   printf("\n The Final result of %d Power %d = %ld", number, exponent, pw);
   return 0;
 }
+//*without power function*//
+#include <stdio.h>
+int main()
+{
+    double base,result = 1;
+    int exp, i;
+    printf("Enter a base number: ");
+    scanf("%lf", &base);
+
+    printf("Enter exponent: ");
+    scanf("%d", &exp);
+
+    for (i=1; i<=exp; i++) {
+        result *= base;
+    }
+    printf("%.2lf^%d = %.2lf", base,exp,result);
+
+    return 0;
+}
